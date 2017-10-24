45dd59c52a12032d2348f4207c572660dab2c67d
唐辉 <tanghui555@live.com>
2017-10-24 21:20:37 +0800

    1

--- queue/QueueT.h
+++ queue/QueueT.h
@@ -13,15 +13,17 @@ typedef struct QueueT {
 	pnode queue;
 	int front;
 	int rear;
+	int max_size;
 }*pqueue;
 /*
 设计一个循环队列
 */
-void init_queue(pqueue p_queue);
-bool enqueue(pqueue p_queue);
-bool dequeue(pqueue p_queue);
-bool is_empty(pqueue p_queue);
-bool is_full(pqueue p_queue);
+extern void init_queue(pqueue p_queue);
+extern bool enqueue(pqueue p_queue,node en_queue);
+bool dequeue(pqueue p_queue,pnode de_queue);
+bool isempty(pqueue p_queue);
+bool isfull(pqueue p_queue);
 void traverse_queue(pqueue p_queue);
+void delete_queue(pqueue p_queue);
 
 #endif
--- queue/源.cpp
+++ queue/源.cpp
@@ -1,15 +1,13 @@
-
 #include "QueueT.h"
 #include <iostream>
 using namespace std;
-
 
 
 int main()
 {
 	QueueT head_queue ;
 	init_queue(&head_queue);
-
-	cout << head_queue.front << endl;
+	isempty(&head_queue);
+	//cout << head_queue.front << endl;
 
 }
